a439589381a683196de95718c7cf6524c2d87e28
Jim Marino <jim.marino@gmail.com>
2025-05-06 12:25:44 +0200

    Implement XRegistry library

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+rootProject.name = "fleet-coordinator"
+
+// this is needed to have access to snapshot builds of plugins
+pluginManagement {
+    repositories {
+        mavenLocal()
+        maven {
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        maven {
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+        }
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+include(":common:xregistry:xregistry-model")
+include(":common:xregistry:xregistry-lib")
+include(":common:xregistry:xregistry-policy")
+include(":common:xregistry:xregistry-schema")
+
