e5faa121a31ee4916ae3ceab390a0454ad17c8e2
Jim Marino <jim.marino@gmail.com>
2025-05-06 14:26:31 +0200

    feat: Add registry server (#3)
    Add registry server

---
+++ registry/launcher/build.gradle.kts
@@ -0,0 +1,40 @@
+/*
+ *  Copyright (c) 2025 Metaform Systems, Inc.
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Metaform Systems - initial API and implementation
+ *
+ */
+
+plugins {
+    `java-library`
+    id("application")
+    alias(libs.plugins.shadow)
+}
+
+dependencies {
+    runtimeOnly(libs.bundles.base.runtime)
+    runtimeOnly(project(":registry:registry-server"))
+    runtimeOnly(project(":registry:registry-policy"))
+    runtimeOnly(project(":registry:registry-policy-memory"))
+}
+
+application {
+    mainClass.set("org.eclipse.edc.boot.system.runtime.BaseRuntime")
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    exclude("**/pom.properties", "**/pom.xml")
+    mergeServiceFiles()
+    archiveFileName.set("registry-server.jar")
+}
+
+edcBuild {
+    publish.set(false)
+}
